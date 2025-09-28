38a7e155583fa5073299fccdb3c35a2b1f9512eb
avinashvishukinesiis <avinash@kinesiis.com>
2025-09-28 23:29:03 +0530

    new changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  webpack: (config:any) => {
+    return config // 👈 forces Webpack build, disables Turbopack
+  },
+}
+
+module.exports = nextConfig
